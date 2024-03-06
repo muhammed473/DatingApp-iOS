d55acfa128724b7fb8adf8f37f137e11c85a383b
muhammed dursun <mamiankara10@gmail.com>
2024-03-06 18:21:22 +0300

    Project started.

---
+++ DatingApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DatingApp
+//
+//  Created by muhammed dursun on 6.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+
+
+}
+
