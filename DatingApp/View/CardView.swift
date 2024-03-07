db6c964ceb14a11f3a03fddf7153a58140dd2b46
muhammed dursun <mamiankara10@gmail.com>
2024-03-08 01:06:10 +0300

    Sliding the card at an angle, and depending on the shifting intensity, the card returns to the starting position or the card is thrown out.

--- DatingApp/View/CardView.swift
+++ DatingApp/View/CardView.swift
@@ -7,6 +7,11 @@
 
 import UIKit
 
+enum DirectionSwipe : Int{
+    case left = -1
+    case right = 1
+}
+
 class CardView : UIView{
     
     // MARK: - Properties
@@ -36,6 +41,7 @@ class CardView : UIView{
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+        configureGestureRecognizers()
         
         backgroundColor = .purple
         layer.cornerRadius = 12
@@ -66,4 +72,61 @@ class CardView : UIView{
         layer.addSublayer(gradientLayer)
     }
     
+    func configureGestureRecognizers() {
+        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(touchPanGesture))
+        addGestureRecognizer(panGesture)
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(touchChangePhoto))
+        addGestureRecognizer(tapGesture)
+    }
+    
+    func rotationPanCard(sender: UIPanGestureRecognizer){
+        let relocate = sender.translation(in: nil)
+        let degrees : CGFloat = relocate.x / 20
+        let angle = degrees * .pi / 180
+        let rotationRelocate = CGAffineTransform(rotationAngle: angle)
+        self.transform = rotationRelocate.translatedBy(x: relocate.x, y: relocate.y)
+    }
+    
+    func resultSwipeIntensity(sender: UIPanGestureRecognizer){
+        let direction : DirectionSwipe = sender.translation(in: nil).x > 100 ? .right : .left
+        let shouldDeclineCard = abs(sender.translation(in: nil).x) > 100
+        UIView.animate(withDuration: 0.75, delay: 0, usingSpringWithDamping: 0.6,initialSpringVelocity: 0.1,options: .curveEaseOut,animations: {
+            if shouldDeclineCard {
+                let xRelocate = CGFloat(direction.rawValue) * 1000
+                let offscreenTransform = self.transform.translatedBy(x: xRelocate, y: 0)
+                self.transform = offscreenTransform
+            }else{
+                self.transform = .identity
+            }
+            
+        }) { _ in
+            print("Animation finish.")
+            if shouldDeclineCard {
+                self.removeFromSuperview()
+            }
+            
+        }
+    }
+    
+    // MARK: - Actions
+    
+    @objc func touchPanGesture(sender:UIPanGestureRecognizer) {
+        switch sender.state{
+        case .began:
+            print("Swipe is starting.")
+            superview?.subviews.forEach({ $0.layer.removeAllAnimations() })
+        case .changed:
+           rotationPanCard(sender: sender)
+        case .ended:
+            resultSwipeIntensity(sender: sender)
+        default: break
+        }
+        
+        
+    }
+    
+    @objc func touchChangePhoto(sender: UITapGestureRecognizer) {
+        print("Fotoğrafa dokunuldu.")
+    }
 }
